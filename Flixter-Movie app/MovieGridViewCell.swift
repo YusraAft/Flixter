f7efade2fdbe37facffba6f6504e4c708b9acb31
Yusra <aftabyusra786@gmail.com>
2021-02-26 00:23:37 -0600

    Collection View Setup

---
+++ Flixter-Movie app/MovieGridViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridViewCell.swift
+//  Flixter-Movie app
+//
+//  Created by Mac User on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flixter-Movie app/MovieGridViewController.swift
+++ Flixter-Movie app/MovieGridViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import  AlamofireImage
 
-class MovieGridViewController: UIViewController {
+class MovieGridViewController: UIViewController, UICollectionViewDataSource,
+                               UICollectionViewDelegate {
+   
+    @IBOutlet var collectionView: UICollectionView!
+    
     var movies = [[String: Any]]() //array of dictionaries
     //every view controller needs it own movies
     
     //prtty common for every screen to start off with viewdidload and a network call
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
 
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -27,7 +35,7 @@ class MovieGridViewController: UIViewController {
 
               self.movies = dataDictionary["results"] as! [[String: Any]] //now download is complete
             //print(self.movies)
-            
+            self.collectionView.reloadData() //can you reload data because I have updated the movies count
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
               // TODO: Reload your table view data
@@ -35,6 +43,20 @@ class MovieGridViewController: UIViewController {
            }
         }
         task.resume()
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridViewCell", for: indexPath) as! MovieGridViewCell
+        let movie = movies[indexPath.item]
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!//like a string but enforces http etc.
+        cell.posterView.af_setImage(withURL: posterUrl)
+        
+        return cell
     }
     
 
